d5cfa244545150d6b31e17ddc96157207923977b
RocWood <rocwood@flu3d.com>
2025-05-06 09:36:11 +0800

    add photino.Blazor and MudBlazor as startup

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+using MudBlazor.Services;
+
+namespace RepoHub
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+            appBuilder.Services
+                .AddLogging()
+                .AddMudServices();
+
+            // register root component and selector
+            appBuilder.RootComponents.Add<App>("app");
+
+            var app = appBuilder.Build();
+
+            // customize window
+            app.MainWindow
+                .SetIconFile("favicon.ico")
+                .SetTitle("RepoHub")
+                .SetWidth(1280)         // 设置默认宽度
+                .SetHeight(800)         // 设置默认高度
+                .Center();              // 窗口居中显示
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+            {
+                app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+            };
+
+            app.Run();
+        }
+    }
+}
