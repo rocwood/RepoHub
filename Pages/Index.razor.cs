e92db964f10c59dff7150d2cced51a59cc24ce99
RocWood <rocwood@flu3d.com>
2025-05-09 16:10:20 +0800

    修复命令行参数以\"结束的解析错误

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -21,25 +21,35 @@ public partial class Index
 
             var settings = AppSettings.Load();
 
-			// 先检查是否指定启动路径
-			var args = Environment.GetCommandLineArgs();
-			if (args != null && args.Length > 1)
-			{
-				var argPath = args[1];
+            // 先检查是否指定启动路径
+            var args = Environment.GetCommandLineArgs();
+            if (args != null && args.Length > 1)
+            {
+                var argPath = args[1];
 
-				if (!string.IsNullOrEmpty(argPath) && Directory.Exists(args[1]))
-				{
-					settings.WorkspacePaths.AddUnique(argPath);
-					settings.LastWorkspacePath = argPath;
-					settings.Save();
-				}
-			}
+                if (!string.IsNullOrEmpty(argPath))
+                {
+                    // 由于命令行参数可能传入"路径\"，可能被错误解释保留了最后的双引号
+                    if (argPath.EndsWith('"'))
+                        argPath = argPath.Substring(0, argPath.Length - 1);
 
-            if (!string.IsNullOrEmpty(settings.LastWorkspacePath) && 
+                    if (argPath.EndsWith('/') || argPath.EndsWith('\\'))
+                        argPath = argPath.Substring(0, argPath.Length - 1);
+
+                    if (Directory.Exists(argPath))
+                    {
+                        settings.WorkspacePaths.AddUnique(argPath);
+                        settings.LastWorkspacePath = argPath;
+                        settings.Save();
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(settings.LastWorkspacePath) &&
                 Directory.Exists(settings.LastWorkspacePath))
             {
                 NavigationManager.NavigateTo("workspace");
             }
         }
     }
-} 
+}
